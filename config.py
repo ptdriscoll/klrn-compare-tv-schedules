56440350b20bd03c7b4b9e22f65391843347d8a5
Patrick Driscoll <pdriscoll@satx.rr.com>
2025-03-10 14:56:31 -0500

    Setup for PBS TV Schedule

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+
 # names of raw files to parse, in format {'parser': 'file_name'}
 FILES = {
     'protrack': 'February 2025 Schedule.pdf', # .pdf format
     'titan': 'MediaStar_9.2.mhtml' # .mhtml format
 }
+
+# variables for call to PBS TV Schedules API
+load_dotenv()
+PBS_TV_SCHEDULE_API_KEY = os.geteven('PBS_TV_SCHEDULE_API_KEY') 
+PBS_TV_SCHEDULE_ENDPOINT = 'https://tvss.services.pbs.org/tvss/'
+STATION_CALL_SIGN = 'klrn'
